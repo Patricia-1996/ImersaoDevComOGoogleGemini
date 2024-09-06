70cbd366ae6180688c02e7aa9f71c1d7cd89bf9d
Patricia-1996 <patricia.escobar@edu.pucrs.br>
2024-09-06 15:39:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "Hiragana",
+        descricao: " あa いi うu えe おo かka きki くku けke こko さsa しshi すsu せse そso たta ちchi つtsu てte とto なna にni ぬnu ねne のno はha ひhi ふfu へhe ほho まma みmi むmu めme もmo やya ゆyu よyo らra りri るru れre ろro わwa をwo んn ",
+        link: "https://www.nhk.or.jp/lesson/pt/letters/hiragana.html",
+        tags: "hiragana kana kanas alfabeto letra japones"
+    },
+    {
+        titulo: "Katakana",
+        descricao: "アa イi ウu エe オo カka キki クku ケke コko サsa シshi スsu セse ソso タta チchi ツtsu テteトto ナna ニni ヌnu  ネne ノno ハha ヒhi フfu ヘhe ホho マma ミmi ムmu メme モmo ヤya ユyu ヨyo ラra リri ルru レre ロro ワwa ヲwo ンn",
+        link: "https://www.nhk.or.jp/lesson/pt/letters/katakana.html",
+        tags: "katakana kana kanas alfabeto letra japones"
+    },
+    {
+        titulo: "Kanji",
+        descricao: `A quantidade exata de kanji existentes é difícil de determinar, pois ao longo da história novos kanji foram criados e outros caíram em desuso. No entanto, para fins práticos e educacionais, o governo japonês estabeleceu uma lista oficial de kanji utilizados.
+Jōyō Kanji é a lista oficial de kanji a serem utilizados em textos oficiais, imprensa, editores e demais meios de difusão da língua escrita japonesa. Essa lista contém 2136 kanji.
+Mas nem todos os 2136 kanji são usados diariamente. A quantidade de kanji que uma pessoa precisa saber varia de acordo com o seu nível de educação e área de atuação. Por exemplo:
+
+* Educação primária: Cerca de 1026 kanji.
+* Educação secundária: Até 1110 kanji.
+* Uso diário: Varia de pessoa para pessoa, mas muitos conseguem se comunicar com uma fração dos 2136 kanji.
+
+É importante ressaltar:
+* Existem muitos kanji raros, utilizados em textos especializados ou antigos.
+* Ao longo do tempo, novos kanji podem ser criados para representar conceitos modernos.
+
+Em resumo:
+* 2136 kanji: Total na lista oficial (Jōyō Kanji).
+* 1026 kanji: Ensino primário.
+* 1110 kanji: Ensino médio.
+* Uso diário: Varia.
+
+Para um aprendizado mais eficaz, comece com os kanji mais comuns e aumente gradualmente seu vocabulário.`,
+        link: "https://www.youtube.com/watch?v=V3HFmqwPkos&pp=ygUFa2Fuamk%3D",
+        tags: "kanji ideograma ideogramas alfabeto letra japones"
+    }
+];
